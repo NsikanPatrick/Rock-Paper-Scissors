11bc6f855aa7f9ce55e20feb07f640c28b243b3d
NsikanPatrick <102978659+NsikanPatrick@users.noreply.github.com>
2022-06-06 15:20:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from random import randint
 
 #create a list of play options
+
+#NB: I use Rock, Paper and Scissors to represent the R, P and S specified in the task since it will be more clear to the player
+
+from random import randint
+
+#create a list of play options
 choices = ["Rock", "Paper", "Scissors"]
 
 #assign a random play to the cpu
@@ -14,23 +20,30 @@ while opponent == False:
     opponent = input("Rock, Paper, Scissors?")
     if opponent == cpu:
         print("Tie!")
+      
     elif opponent == "Rock":
+      print("CPU move=>", cpu, "while Player move=>", opponent)
         if cpu == "Paper":
             print("You lose!", cpu, "covers", opponent)
         else:
             print("You win!", opponent, "smashes", cpu)
+          
     elif opponent == "Paper":
-        if cpu == "Scissors":
+      print("CPU move=>", cpu, "while Player move=>", opponent)
+        if cpu == "Scissors":            
             print("You lose!", cpu, "cuts", opponent)
         else:
             print("You win!", opponent, "covers", cpu)
+          
     elif opponent == "Scissors":
+      print("CPU move=>", cpu, "while Player move=>", opponent)
         if cpu == "Rock":
             print("You lose...", cpu, "smashes", opponent)
         else:
             print("You win!", opponent, "cuts", cpu)
     else:
-        print("That's an invalid input. Ensure your input is initial capitalized!")
-    #opponent was set to True, but we want it to be False so the loop continues
+        print("Your input is invalid. Ensure your input is initial capitalized!")
+        
+    #opponent was set to True, but i change it to be False so the loop continues
     opponent = False
     cpu = choices[randint(0,2)]
